50f05346d5315ead8ce71cc85d7b9b93cb4e118f
Luiz Sousa <luiz.sousa@netcracker.com>
2023-06-06 16:41:53 -0300

    create routes and prepare jwt

---
+++ src/http/routes/routes.ts
@@ -0,0 +1,18 @@
+import { FastifyInstance } from 'fastify'
+import { addPetRoute } from './pets/add'
+import { petDetailsRoute } from './pets/details'
+import { searchPetRoute } from './pets/search'
+import { signInRoute } from './register/session'
+import { createUserRoute } from './register/create-user'
+import { verifyJwt } from '../middlewares/verify-jwt'
+
+export async function userRoutes(app: FastifyInstance) {
+  app.post('/', createUserRoute)
+  app.post('/login', signInRoute)
+}
+
+export async function petRoutes(app: FastifyInstance) {
+  app.get('/', searchPetRoute)
+  app.get('/:id', petDetailsRoute)
+  app.post('/', { onRequest: [verifyJwt] }, addPetRoute)
+}
